fd74837fe9293c53b8e5d7852d320a9a60d96dc6
zshell.zhang <zshell.zhang@qunar.com>
2017-01-19 11:53:49 +0800

    init

---
+++ src/main/scala/com/qunar/spark/transmit/Main.scala
@@ -0,0 +1,8 @@
+package com.qunar.spark.transmit
+
+/**
+  * Created by zshell on 17-1-19.
+  */
+class Main {
+
+}
---
+++ src/main/scala/com/qunar/spark/transmit/bean/TaskSnapshot.scala
@@ -0,0 +1,7 @@
+package com.qunar.spark.transmit.bean
+
+/**
+  * 任务信息的快照
+  */
+case class TaskSnapshot(index: String,
+                        `type`: String) {}
