049d1c38441e7f7df7da9943f76ee2dce4f6ca2d
zshell.zhang <zshell.zhang@qunar.com>
2017-02-01 00:14:31 +0800

    task logging

--- data_transmit_client/src/main/scala/com/qunar/spark/transmit/Task.scala
+++ data_transmit_client/src/main/scala/com/qunar/spark/transmit/Task.scala
@@ -1,26 +1,53 @@
 package com.qunar.spark.transmit
 
+import com.qunar.spark.base.io.HdfsService
+import com.qunar.spark.base.json.JsonMapper
+import com.qunar.spark.base.log.Logging
 import com.qunar.spark.transmit.phase._
+import org.apache.hadoop.fs.Path
+import org.joda.time.DateTime
 
-import scala.language.implicitConversions
+import scala.collection.immutable.HashMap
 
 /**
   * 任务信息的快照,用以生成执行计划
   */
-class Task private[transmit](private val exportPhase: ExportPhase,
-                             private val importPhase: ImportPhase) {
+class Task private[transmit](private val taskName: String,
+                             private val exportPhase: ExportPhase,
+                             private val importPhase: ImportPhase) extends Logging {
 
   /**
-    * 开始传输数据
+    * 提交传输数据的任务快照至Daemon服务,触发服务执行任务
     */
   def transmitData(): Unit = {
-    val exportPhasePlan = exportPhase.genPhaseExecutionPlan
-    val importPhasePlan = importPhase.genPhaseExecutionPlan
-    val taskExecutionPlan = TaskExecutionPlan(exportPhasePlan, importPhasePlan)
-    // 写入执行计划,通知Daemon服务执行任务
-    if (!taskExecutionPlan.writeToTransLog) {
-      //todo: extends Logging and log error here
+    if (!writeToTransLog(exportPhase, importPhase)) {
+      logError(s"taskExecutionPlan writeToTransLog fail\n " +
+        s"exportPhasePlan = ${JsonMapper.writeValueAsString(exportPhase.genPhaseExecutionPlan)}\n" +
+        s"importPhasePlan = ${JsonMapper.writeValueAsString(importPhase.genPhaseExecutionPlan)}")
     }
+  }
+
+  /**
+    * 将执行计划写入事务预写日志
+    *
+    * @return 是否预写成功
+    */
+  private def writeToTransLog(exportPhase: ExportPhase, importPhase: ImportPhase): Boolean = {
+    val exportPhasePlan = exportPhase.genPhaseExecutionPlan
+    val exportPhaseType = exportPhase.phaseType
+    val importPhasePlan = importPhase.genPhaseExecutionPlan
+    val importPhaseType = importPhase.phaseType
+    val taskInfo = new TaskInfo(exportPhaseType, exportPhasePlan, importPhaseType, importPhasePlan)
+
+    writeToTransLogInternal(taskInfo)
+  }
+
+  /**
+    * 内部具体实现hdfs落地的逻辑
+    */
+  private def writeToTransLogInternal(taskInfo: TaskInfo): Boolean = {
+    val transLogPath = "/data-transmit/translog/" + DateTime.now().toString("yyyyMMddHHmmss") + "/" + taskInfo
+    HdfsService.writeContentToPath(new Path(transLogPath), JsonMapper.writeValueAsString(taskInfo))
   }
 
 }
@@ -31,9 +58,15 @@ object Task {
 
   class TaskBuilder {
 
+    private var taskName: String = _
+
     private var exportPhaseBuilder: ExportPhaseBuilder = _
 
     private var importPhaseBuilder: ImportPhaseBuilder = _
+
+    def setTaskName(taskName: String) = {
+      this.taskName = taskName
+    }
 
     def elasticsearchExportPhaseBuilder = {
       val taskPhaseBuilder = TaskPhaseBuilder.elasticSearchExportPhaseBuilder(this)
@@ -64,9 +97,60 @@ object Task {
     }
 
     def buildTask: Task = {
-      new Task(exportPhaseBuilder.buildPhase, importPhaseBuilder.buildPhase)
+      new Task(taskName, exportPhaseBuilder.buildPhase, importPhaseBuilder.buildPhase)
     }
 
+  }
+
+}
+
+/**
+  * 任务信息的标准java bean,与daemon服务通信的唯一媒介
+  * 通过将其写入hdfs,引起daemon端监听服务active,执行任务
+  * NOTICE: 这里不使用scala case class,是考虑到jackson的兼容性限制.因为
+  * 采用jackson序列化,使用标准的java bean是最稳定的.
+  */
+class TaskInfo(exportType: ExportPhaseType,
+               exportInfo: HashMap[String, String],
+               importType: ImportPhaseType,
+               importInfo: HashMap[String, String]) {
+
+  def this() = this(null, null, null, null)
+
+  // 数据导出阶段的类型
+  private var exportPhaseType: ExportPhaseType = exportType
+
+  // 数据导出阶段的信息
+  private var exportPhaseInfo: HashMap[String, String] = exportInfo
+
+  // 数据导入阶段的类型
+  private var importPhaseType: ImportPhaseType = importType
+
+  // 数据导入阶段的信息
+  private var importPhaseInfo: HashMap[String, String] = importInfo
+
+  def getExportPhaseType = exportPhaseType
+
+  def setExportPhaseType(exportPhaseType: ExportPhaseType) = {
+    this.exportPhaseType = exportPhaseType
+  }
+
+  def getExportPhaseInfo = exportPhaseInfo
+
+  def setExportPhaseInfo(exportPhaseInfo: HashMap[String, String]) = {
+    this.exportPhaseInfo = exportPhaseInfo
+  }
+
+  def getImportPhaseType = importPhaseType
+
+  def setImportPhaseType(importPhaseType: ImportPhaseType) = {
+    this.importPhaseType = importPhaseType
+  }
+
+  def getImportPhaseInfo = importPhaseInfo
+
+  def setImportPhaseInfo(importPhaseInfo: HashMap[String, String]) = {
+    this.importPhaseInfo = importPhaseInfo
   }
 
 }
--- data_transmit_client/src/main/scala/com/qunar/spark/transmit/TaskExecutionPlan.scala
+++ data_transmit_client/src/main/scala/com/qunar/spark/transmit/TaskExecutionPlan.scala
@@ -9,15 +9,7 @@ import scala.collection.immutable.HashMap
 class TaskExecutionPlan private[transmit](private val exportPhaseInfo: HashMap[String, String],
                                           private val importPhaseInfo: HashMap[String, String]) {
 
-  /**
-    * 将执行计划写入事务预写日志
-    *
-    * @return 是否预写成功
-    */
-  def writeToTransLog: Boolean = {
-    //todo jackson serialize task info
-    false
-  }
+
 
 }
 
@@ -29,38 +21,4 @@ object TaskExecutionPlan {
 
 }
 
-class TaskInfo {
 
-  private var exportPhaseType: ExportPhaseType = _
-
-  private var exportPhaseInfo: HashMap[String, String] = _
-
-  private var importPhaseType: ImportPhaseType = _
-
-  private var importPhaseInfo: HashMap[String, String] = _
-
-  def getExportPhaseType = exportPhaseType
-
-  def setExportPhaseType(exportPhaseType: ExportPhaseType) = {
-    this.exportPhaseType = exportPhaseType
-  }
-
-  def getExportPhaseInfo = exportPhaseInfo
-
-  def setExportPhaseInfo(exportPhaseInfo: HashMap[String, String]) = {
-    this.exportPhaseInfo = exportPhaseInfo
-  }
-
-  def getImportPhaseType = importPhaseType
-
-  def setImportPhaseType(importPhaseType: ImportPhaseType) = {
-    this.importPhaseType = importPhaseType
-  }
-
-  def getImportPhaseInfo = importPhaseInfo
-
-  def setImportPhaseInfo(importPhaseInfo: HashMap[String, String]) = {
-    this.importPhaseInfo = importPhaseInfo
-  }
-
-}
