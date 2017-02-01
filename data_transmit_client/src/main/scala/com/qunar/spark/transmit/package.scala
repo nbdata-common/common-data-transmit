2dbd581af80a8c7f946bd8c6123346999357dd30
zshell.zhang <zshell.zhang@qunar.com>
2017-02-01 23:16:25 +0800

    package.info

--- data_transmit_client/src/main/scala/com/qunar/spark/transmit/package.scala
+++ data_transmit_client/src/main/scala/com/qunar/spark/transmit/package.scala
@@ -3,6 +3,7 @@ package com.qunar.spark
 import com.qunar.spark.transmit.phase.TaskPhaseType
 
 /**
+  * <span class="badge" style="float: right;">ALPHA COMPONENT</span>
   */
 package object transmit {
 
